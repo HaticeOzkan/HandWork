a80930863fd3cb91ac0d36d66d4fd5d9e3b94bb2
HaticeOzkan <46075358+HaticeOzkan@users.noreply.github.com>
2019-03-12 20:24:23 +0300

    edit

--- HandWork/HandWork/Controllers/ProductController.cs
+++ HandWork/HandWork/Controllers/ProductController.cs
@@ -16,13 +16,14 @@ namespace HandWork.Controllers
 
         public ActionResult Index(int id)
         {
-          
+
             return View();
         }
         [HttpGet]
         public ActionResult AddProduct()
         {
-            ViewBag.Categories = _uw.CategoryRepo.GetAll().Select(x => new SelectListItem {
+            ViewBag.Categories = _uw.CategoryRepo.GetAll().Select(x => new SelectListItem
+            {
                 Text = x.CategoryName,
                 Value = (x.ID).ToString()
             });
@@ -30,7 +31,7 @@ namespace HandWork.Controllers
 
         }
         [HttpPost]
-        public ActionResult AddProduct(Product NewProduct,HttpPostedFileBase[] images)
+        public ActionResult AddProduct(Product NewProduct, HttpPostedFileBase[] images)
         {
             ViewBag.Categories = _uw.CategoryRepo.GetAll().Select(x => new SelectListItem
             {
@@ -39,35 +40,61 @@ namespace HandWork.Controllers
             });
             if (ModelState.IsValid)
             {
-                NewProduct.MemberID = User.Identity.GetUserId();           
-            if (images != null)
-            {
+                NewProduct.MemberID = User.Identity.GetUserId();
+                if (images != null)
+                {
                     int Count = 0;
-                  NewProduct.ProductImages = new List<ProductImage>();
-                foreach (HttpPostedFileBase item in images)
-                {                 
-                    ProductImage NewImage = new ProductImage();
-                    string Path = Server.MapPath("/Uploads/Products/");//dosya yolu
-                    item.SaveAs(Path + Count + ".jpg");//image ismi                 
-                    NewImage.ImageURL = "/Uploads/Products/" + Count + ".jpg";                     
-                    NewProduct.ProductImages.Add(NewImage);
+                    NewProduct.ProductImages = new List<ProductImage>();
+                    foreach (HttpPostedFileBase item in images)
+                    {
+                        ProductImage NewImage = new ProductImage();
+                        string Path = Server.MapPath("/Uploads/Products/");//dosya yolu
+                        item.SaveAs(Path + Count + ".jpg");//image ismi                 
+                        NewImage.ImageURL = "/Uploads/Products/" + Count + ".jpg";
+                        NewProduct.ProductImages.Add(NewImage);
                         Count++;
 
                     }
-                NewProduct.HasPhoto = true;             
+                    NewProduct.HasPhoto = true;
                     _uw.ProductRepo.Add(NewProduct);
                     _uw.Complete();
                 }
-          
-            return View();
+
+                return RedirectToAction("MemberProfile", "Member");
             }
-            return View();
-        }
-        public ActionResult DeleteProduct()
-        {
-            string ID = User.Identity.GetUserId();
-          
             return RedirectToAction("MemberProfile", "Member");
         }
+        public ActionResult DeleteProduct(int ProductID)
+        {
+
+            _uw.ProductRepo.Delete(ProductID);
+
+            return RedirectToAction("MemberProfile", "Member");
+        }
+        [HttpGet]
+        public ActionResult EditProduct(int ProductID)
+        {
+            ViewBag.Categories = _uw.CategoryRepo.GetAll().Select(x => new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = (x.ID).ToString()
+            });
+            Product Product = _uw.ProductRepo.GetOne(ProductID);
+            ViewBag.ImageList = _uw.Db.ProductImages.Where(x => x.Product.ID == ProductID).Select(x => x.ImageURL).ToList();
+
+
+            return View(Product);
+        }
+        [HttpPost]
+        public ActionResult EditProduct(Product NewProduct, HttpPostedFileBase[] images)
+        {
+            ViewBag.Categories = _uw.CategoryRepo.GetAll().Select(x => new SelectListItem
+            {
+                Text = x.CategoryName,
+                Value = (x.ID).ToString()
+            });
+            return View();
+        }
+
     }
 }
